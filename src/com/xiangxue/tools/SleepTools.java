b1a339b3fb1c6ca243ca9b92e63a925b6afbbc22
liangya <liangya@yuanbaobaoxian.com>
2021-04-27 17:29:19 +0800

    提交时间工具

--- src/com/xiangxue/tools/SleepTools.java
+++ src/com/xiangxue/tools/SleepTools.java
@@ -1,7 +1,32 @@
-package com.xiangxue.tools;/**
- * 
+package com.xiangxue.tools;
+
+import java.util.concurrent.TimeUnit;
+
+/**
  * @author liangya
  * @date 2021/4/27 16:45
+ * 跟着老师敲一个睡眠的工具
  */
 public class SleepTools {
+    /**
+     * 按照秒休眠
+     */
+    public static final void second(int seconds){
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * 按照毫秒休眠
+     */
+    pulic static final void ms(int seconds){
+        try {
+            TimeUnit.MILLISECONDS.sleep(seconds);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
 }
